a12513cd54bfbf0a79085091d0b5086e6190a46a
Sayan Naskar <47991790+sayann002@users.noreply.github.com>
2020-09-28 00:30:57 +0530

    Updates

---
+++ precache-manifest.eb2454f8a3f09f06e717f0e4219b768f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1542a1b31cfeac69607d368793a955",
+    "url": "/React-ToDo-List/index.html"
+  },
+  {
+    "revision": "c06896449346a228ce47",
+    "url": "/React-ToDo-List/static/css/main.dbeb7705.chunk.css"
+  },
+  {
+    "revision": "e8c5f8b2cd6b71a8a1e1",
+    "url": "/React-ToDo-List/static/js/2.7372f633.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ToDo-List/static/js/2.7372f633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06896449346a228ce47",
+    "url": "/React-ToDo-List/static/js/main.2b20b312.chunk.js"
+  },
+  {
+    "revision": "a814aba498572478370d",
+    "url": "/React-ToDo-List/static/js/runtime-main.ed8fa3d1.js"
+  }
+]);
